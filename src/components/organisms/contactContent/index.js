9f5f12b5b8c728470f7ea08b45516ae9d80ea284
Justyna Ciesla <justyna.ciesla@people-analytix.com>
2023-04-26 15:39:26 +0200

    changes

--- src/components/organisms/contactContent/index.js
+++ src/components/organisms/contactContent/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyledWrapper } from "./StyledContactContent";
+import { StyledWrapper, StyledLine } from "./StyledContactContent";
 import ContactCircle from "../../molecules/contactCircle";
 import ContactForm from "../../molecules/contactForm";
 import Line from "../../atoms/line";
